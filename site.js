92c010d33652cb3fef84f107eb440cbd465867f3
nieldlr <nieldlr@gmail.com>
2016-01-30 07:46:25 -1000

    First commit

---
+++ site.js
@@ -0,0 +1,11 @@
+$(function(){
+  // var height = $(window).height();
+
+  // $('.content-wrapper').css({'min-height':((height))+'px', 'border-bottom':'0'});
+
+  // $(window).resize(function(){
+  //   var height = $(window).height();
+  //   $('.content-wrapper').css({'min-height':((height))+'px'});
+  // });
+});
+  
